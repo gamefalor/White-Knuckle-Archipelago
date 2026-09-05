dcb434e4a37359e9bc733916ff4504e2bd096d98
Makshime <157326516+Makshime@users.noreply.github.com>
2026-09-05 00:38:15 -0700

    Attempting to merge some stuff

--- APWorld/whiteknuckle/options.py
+++ APWorld/whiteknuckle/options.py
@@ -22,20 +22,51 @@ class Deathlink_Amnesty(NamedRange):
     unchanged means it wont change the setting in the client upon first connection
     """
 
-    display_name = "Deathlink"
+    display_name = "Deathlink Amnesty"
     special_range_names = {
     "unchanged": 0,
-    }
+    
+    
     range_start = 0
-    range_end = 100
-    default = 0
+    range_end = 20
+    default = 2
+    }
+
+class TotalBuffs(Range):
+    """
+    The amount of "Progressive Stats" will be in the pool
+    One buff is worth the opposite of a debuff
+    """
+
+    display_name = "Total Buffs"
+
+    range_start = 0
+    range_end = 25
+    default = 15
+
+class StartingDebuffs(Range):
+    """
+    The amount of "Debuff" perks you start with
+    """
+
+    display_name = "Starting Debuffs"
+    range_start = 0
+    range_end = 20
+    default = 10
+    
 
 @dataclass
 class WKOptions(PerGameCommonOptions):
+    Starting_Debuffs: StartingDebuffs
+    Total_Buffs: TotalBuffs
     deathlink: Deathlink
     deathlink_amnesty: Deathlink_Amnesty
 
 option_groups = [
+    OptionGroup(
+        "Difficulty",
+        [StartingDebuffs, TotalBuffs],
+    ),
     OptionGroup(
         "Client_Side",
         [Deathlink, Deathlink_Amnesty],
